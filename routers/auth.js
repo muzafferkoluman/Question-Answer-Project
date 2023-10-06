e0e207cc104249e255a60fdf7cd8f3ee1c5c2141
Muzaffer Koluman <muzafferkoluman@Victo-MacBook-Pro.local>
2023-10-07 01:26:01 +0200

    controllers pages added

--- routers/auth.js
+++ routers/auth.js
@@ -5,9 +5,7 @@ const router = express.Router()
 router.get("/", (req, res) => {
     res.send("Auth Page")
 })
-router.get("/register", (req, res) => {
-    res.send("Auth Register Page")
-})
+
 
 
 module.exports = router
--- routers/question.js
+++ routers/question.js
@@ -5,9 +5,6 @@ const router = express.Router()
 router.get("/", (req, res) => {
     res.send("Questions Page")
 })
-router.get("/delete", (req, res) => {
-    res.send("Questions Delete Page")
-})
 
 
 module.exports = router
