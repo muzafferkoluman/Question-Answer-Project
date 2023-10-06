193d4635948ef653dadb7b6fc829eb833026811b
Muzaffer Koluman <muzafferkoluman@Victo-MacBook-Pro.local>
2023-10-06 17:08:12 +0200

    new pages added

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 const express = require("express")
-const {accessControl,defaultMiddleware} = require("./middleware")
+const dotenv = require("dotenv")
+const { accessControl, defaultMiddleware } = require("./middleware")
 const app = express()
-const PORT = 502
 
-const array = [
-    { id: "1", name: "muzaffer", departmen: "IT", salary: 5000 },
-    { id: "2", name: "koluman", departmen: "IBsT", salary: 2000 }
-]
+// Environment Variables
+dotenv.config({
+    path: "./config/env/config.env"
+})
+const PORT = 502 || process.env.PORT
 
-app.get("/users",accessControl,defaultMiddleware, (req, res, next) => {
-    res.json(array)
+app.get("/", (req, res, next) => {
+    res.send("<h1>HomePage</h1>")
 })
-app.get("/pro", (req, res, next) => {
-    res.send("<h1>Middleware</h1>")
+app.get("/api/questions", (req, res, next) => {
+    res.send("Questions")
+})
+app.get("/api/questions/delete", (req, res, next) => {
+    res.send("Questions Delete")
+})
+app.get("/api/auth", (req, res, next) => {
+    res.send("Auth")
+})
+app.get("/api/auth", (req, res, next) => {
+    res.send("Auth Register")
 })
 
-app.post("/users",(req,res,next)=>{
-    console.log(parseInt(req.body))
-    res.send("<h1>Middleware Post</h1>")
-})
+
+
 
 app.listen(PORT, () => {
-    console.log("Server Started PORT: " + PORT)
+    console.log(`Server Started PORT: ${PORT}  ${process.env.NODE_ENV}`)
 })
+
+
+
+// const array = [
+//     { id: "1", name: "muzaffer", departmen: "IT", salary: 5000 },
+//     { id: "2", name: "koluman", departmen: "IBsT", salary: 2000 }
+// ]
+
+// app.get("/users",accessControl,defaultMiddleware, (req, res, next) => {
+//     res.json(array)
+// })
+
+// app.post("/users",(req,res,next)=>{
+//     console.log(parseInt(req.body))
+//     res.send("<h1>Middleware Post</h1>")
+// })
+
