3f4967d03ef016dc803072989588c02bc9236eba
Muzaffer Koluman <muzafferkoluman@Victo-MacBook-Pro.local>
2023-10-14 17:34:27 +0200

    mongodb validation fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,30 @@
-const register = (req,res,next)=>{
+const User = require("../models/User")
+
+const register = async (req,res,next)=>{
+    // POST DATA
+    const name = "Kaemil"
+    const password = "123456"
+    const email = "rqweeaswqeqw@ad.se"
+
+    // async, await
+
+    const user = await User.create(
+        {
+            name:name,
+            password:password,
+            email:email,
+            role:"admin"
+        }
+    )
+
+
+
     res
     .status(404)
     .json({
-        success:false,
-        name:"Hatali"
+        success:true,
+        data : user
+
     })
 
 }
--- models/User.js
+++ models/User.js
@@ -1,54 +1,56 @@
 const mongoose = require("mongoose")
 
-const userSchema = mongoose.Schema({
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
     name: {
-        type: true,
+        type: String,
         required: [true, "Please provide a name"]
     },
-    email : {
-        type : String,
-        requred : true,
-        unique : [true, "Please try different email"],
-        match : [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            "Please provide a valid name"
-        ],
-        role : {
-            type : String,
-            default : "user",
-            enum : ["user","admin"]
-        },
-        password : {
-            type :String ,
-            minlenght : [6,"Please provide a password with min lenght 6"],
-            required : [true , "Please provide a password"],
-            select : false //hides this in the response data but can be used in commands
-        },
-        createAt:{
-            type : Date,
-            default : Date.now,
-        },
-        title : {
-            type : String
-        },
-        about : {
-            type : String
-        },
-        place : {
-            type : String
-        },
-        website : {
-            type : String
-        },
-        profile_image :{
-            type : String,
-            default : "default.jpg"
-        },
-        blocked : {
-            type : Boolean,
-            default : false
-        }
+    email: {
+        type: String,
+        requred: true,
+        unique: [true, "Please try different email"],
+        //        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w
+
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    },
+    password: {
+        type: String,
+        minlenght: [6, "Please provide a password with min lenght 6"],
+        required: [true, "Please provide a password"],
+        select: false //hides this in the response data but can be used in commands
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+    title: {
+        type: String
+    },
+    about: {
+        type: String
+    },
+    place: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    profile_image: {
+        type: String,
+        default: "default.jpg"
+    },
+    blocked: {
+        type: Boolean,
+        default: false
+
     }
 })
 
-module.exports = mongoose.model("User",userSchema)
+
+module.exports = mongoose.model("User", userSchema)
